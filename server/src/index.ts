55edd9651ad01898f3e24a224bfd9c3fdf34fce2
6ixfalls <bryan888red@gmail.com>
2021-10-24 12:42:06 -0700

    Add ratelimit settings, redo url paths

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,67 +19,93 @@ const db = getFirestore();
 const app = express();
 
 // host static files
-app.use(express.static(path.join(__dirname, "../../client/")));
+app.use("static", express.static(path.join(__dirname, "../../client/static/")));
 app.use(json());
 
 // API handler
 const postLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
     max: 2, // limit each IP to 2 requests per windowMs
+    message: {
+        status: 429,
+        message: undefined,
+        error: "Too many requests, please try again later.",
+    },
 });
 
 const commentLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
     max: 4, // limit each IP to 4 requests per windowMs
+    message: {
+        status: 429,
+        message: undefined,
+        error: "Too many requests, please try again later.",
+    },
+});
+
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/index.html"));
+});
+
+app.get("/posts", async (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/posts.html"));
 });
 
 app.post("/api/comment", commentLimiter, async (req, res) => {
-    const postRef = db.collection("posts").doc(req.body.postId);
-    const post = await postRef.get();
+    try {
+        const postRef = db.collection("posts").doc(req.body.postId);
+        const post = await postRef.get();
 
-    if (!post.exists) {
-        res.status(404).send("Post not found");
-    } else {
+        if (!post.exists) {
+            res.status(404).send({ error: "Post not found!" });
+        } else {
+            const window = new JSDOM("").window;
+            // typescript cant cast from jsdom window to dompurify window, so ignoring errors
+            // @ts-ignore
+            const DOMPurify = createDOMPurify(window);
+
+            const markdownParsed = DOMPurify.sanitize(marked(req.body.content));
+
+            const comment = {
+                author: req.body.author,
+                content: markdownParsed,
+                createdAt: Timestamp.fromDate(new Date()),
+            };
+
+            postRef.update({
+                comments: FieldValue.arrayUnion(comment),
+            });
+
+            res.status(200).send({});
+        }
+    } catch (e) {
+        res.status(500).send({ error: "Internal server error occured." });
+    }
+});
+
+app.post("/api/post", postLimiter, async (req, res) => {
+    try {
+        const postRef = db.collection("posts").doc();
+
         const window = new JSDOM("").window;
-        // typescript cant cast from jsdom window to this window, so ignoring errors
+        // typescript cant cast from jsdom window to dompurify window, so ignoring errors
         // @ts-ignore
         const DOMPurify = createDOMPurify(window);
 
         const markdownParsed = DOMPurify.sanitize(marked(req.body.content));
 
-        const comment = {
-            author: req.body.author,
+        await postRef.set({
+            title: req.body.title,
             content: markdownParsed,
+            author: req.body.author,
+            comments: [],
             createdAt: Timestamp.fromDate(new Date()),
-        };
-
-        postRef.update({
-            comments: FieldValue.arrayUnion(comment),
         });
 
-        res.status(200).send("Comment added");
+        res.status(200).send({ postId: postRef.id });
+    } catch (e) {
+        res.status(500).send({ error: "Internal server error occured." });
     }
-});
-
-app.post("/api/post", postLimiter, async (req, res) => {
-    const postRef = db.collection("posts").doc();
-
-    const window = new JSDOM("").window;
-    // typescript cant cast from jsdom window to this window, so ignoring errors
-    // @ts-ignore
-    const DOMPurify = createDOMPurify(window);
-
-    const markdownParsed = DOMPurify.sanitize(marked(req.body.content));
-
-    await postRef.set({
-        title: req.body.title,
-        content: markdownParsed,
-        author: req.body.author,
-        comments: [],
-        createdAt: Timestamp.fromDate(new Date()),
-    });
-
-    res.status(200).send({ postId: postRef.id });
 });
 
 app.listen(80, () => {
