b5e454819938131001c45d11eded513517147a06
6ixfalls <bryan888red@gmail.com>
2021-10-24 12:15:44 -0700

    Finish serverside (backend)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,87 @@
 import express from "express";
 import path = require("path");
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
+import rateLimit from "express-rate-limit";
+import createDOMPurify from "dompurify";
+import marked from "marked";
+import { json } from "body-parser";
+import { JSDOM } from "jsdom";
 
-const app = express();
+// setup express & firebase
+const serviceAccount = require(path.join(__dirname, "../admin-sdk.json"));
 
-app.use(express.static(path.join(__dirname, "../../client/build")));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/index.html"));
+initializeApp({
+    credential: cert(serviceAccount),
 });
 
-app.get("/")
+const db = getFirestore();
+const app = express();
+
+// host static files
+app.use(express.static(path.join(__dirname, "../../client/")));
+app.use(json());
+
+// API handler
+const postLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 2, // limit each IP to 2 requests per windowMs
+});
+
+const commentLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 4, // limit each IP to 4 requests per windowMs
+});
+
+app.post("/api/comment", commentLimiter, async (req, res) => {
+    const postRef = db.collection("posts").doc(req.body.postId);
+    const post = await postRef.get();
+
+    if (!post.exists) {
+        res.status(404).send("Post not found");
+    } else {
+        const window = new JSDOM("").window;
+        // typescript cant cast from jsdom window to this window, so ignoring errors
+        // @ts-ignore
+        const DOMPurify = createDOMPurify(window);
+
+        const markdownParsed = DOMPurify.sanitize(marked(req.body.content));
+
+        const comment = {
+            author: req.body.author,
+            content: markdownParsed,
+            createdAt: Timestamp.fromDate(new Date()),
+        };
+
+        postRef.update({
+            comments: FieldValue.arrayUnion(comment),
+        });
+
+        res.status(200).send("Comment added");
+    }
+});
+
+app.post("/api/post", postLimiter, async (req, res) => {
+    const postRef = db.collection("posts").doc();
+
+    const window = new JSDOM("").window;
+    // typescript cant cast from jsdom window to this window, so ignoring errors
+    // @ts-ignore
+    const DOMPurify = createDOMPurify(window);
+
+    const markdownParsed = DOMPurify.sanitize(marked(req.body.content));
+
+    await postRef.set({
+        title: req.body.title,
+        content: markdownParsed,
+        author: req.body.author,
+        comments: [],
+        createdAt: Timestamp.fromDate(new Date()),
+    });
+
+    res.status(200).send({ postId: postRef.id });
+});
+
+app.listen(80, () => {
+    console.log(`Listening on port 80!`);
+});
